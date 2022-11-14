7190bde1827ae168db0c64407848350d05852fd0
Liza Marie-Aure (Consultant) <marie-aure.liza@consultant.volvo.com>
2022-11-14 16:56:03 +0100

    [T011] ajoute tableau mocké liste etab dans la page franchise et cree composant creer établissement

---
+++ hsimR_frontend/src/app/model/etablissement.ts
@@ -0,0 +1,14 @@
+export class Etablissement {
+
+    id: number;
+    nom: string;
+    type: string;
+
+
+    constructor( id: number, nom: string, type: string){
+        this.id = id;
+        this.nom = nom;
+        this.type = type;
+    }
+
+}
